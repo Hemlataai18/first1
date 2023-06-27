0c4044f4b0d218f51a15fc60f788bc0e6cca3c04
Hemlataai18 <hemlatachouhan>
2023-06-27 17:31:05 +0530

    all file

---
+++ fitt71.js
@@ -0,0 +1,10 @@
+function text(){
+
+var oth = document.getElementById("1").value;
+alert(oth);
+
+var ma = document.getElementById('m');
+var fe = document.getElementById('f');
+var s = document.getElementById('sb');
+
+}
